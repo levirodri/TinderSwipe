2c5ec7352f7cb9961d2cbbb83d6b042fb7d35253
Levi Rodrigues <levisaj369@gmail.com>
2025-01-22 19:01:21 -0300

    Initial Commit

---
+++ TinderSwipe/TinderSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwipeApp.swift
+//  TinderSwipe
+//
+//  Created by Levi Rodrigues on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
